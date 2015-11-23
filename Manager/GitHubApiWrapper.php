749d5ccafd4df216f0978967284fcec3112f784f
Christian Flach <cmfcmf@users.noreply.github.com>
2015-11-23 21:02:31 +0100

    Attempt another workaround. @craigh

--- Manager/GitHubApiWrapper.php
+++ Manager/GitHubApiWrapper.php
@@ -85,6 +85,9 @@ class GitHubApiWrapper
                 '1.0.0-rc1'
             ];
         }
+        
+        // @todo Fix this.
+        return [1.4.1];
 
         /**
          * @var $semVerVersions version[]
@@ -114,14 +117,13 @@ class GitHubApiWrapper
                 $lastVersion = $currentVersion;
                 $currentVersion = self::versionToMajorMinorPatch($version);
             }
-            // @todo Temporarily disabled the checks.
-            //if ($currentPreRelease = self::versionIsPreRelease($version)) {
+            if ($currentPreRelease = self::versionIsPreRelease($version)) {
                 // Seems like the newest released version is a pre release.
                 // Allow to either release the final version or another pre release.
                 $allowedCoreVersions[] = new version(self::versionToMajorMinorPatch($version));
                 $allowedCoreVersions[] = new version(self::versionToMajorMinorPatch($version) . "-rc" . ++$currentPreRelease);
             
-            //} else {
+            } else {
                 // This is a full version. Allow to release a higher version if the previous version isn't equal to
                 // the higher one.
                 if (!is_int($version->getPatch())) {
@@ -132,7 +134,7 @@ class GitHubApiWrapper
                 if ($newVersion != $lastVersion) {
                     $allowedCoreVersions[] = new version($newVersion . "-rc1");
                 }
-            //}
+            }
         }
         usort($allowedCoreVersions, function (version $a, version $b) {
             return version::rcompare($a, $b);
