07adc062adcde75a61b6d1333702083fa749b7a0
Craig Heydenburg <craigh@mac.com>
2017-07-18 09:18:10 -0400

    convert passwordtype to texttype

--- Form/Type/ConfigType.php
+++ Form/Type/ConfigType.php
@@ -4,7 +4,6 @@ namespace Zikula\Module\CoreManagerModule\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -55,13 +54,11 @@ class ConfigType extends AbstractType
                 'label' => $this->__('Core repository'),
                 'help' => $this->__('Fill in the name of the core repository. This should always be "zikula/core"')
             ])
-            ->add('github_token', PasswordType::class, [
-                'always_empty' => false,
+            ->add('github_token', TextType::class, [
                 'label' => $this->__('Access Token'),
                 'help' => $this->__f('Create a personal access token at %s to raise your api limits.', ['%s' => '<a href="https://github.com/settings/applications">https://github.com/settings/applications</a>'])
             ])
-            ->add('github_webhook_token', PasswordType::class, [
-                'always_empty' => false,
+            ->add('github_webhook_token', TextType::class, [
                 'label' => $this->__('Webhook Security Token'),
                 'help' => $this->__f('Create a secrete webhook token at %s to verify payloads from the Zikula Core repository.', ['%s' => '<a href="https://developer.github.com/webhooks/securing">https://developer.github.com/webhooks/securing/</a>'])
             ])
@@ -69,8 +66,7 @@ class ConfigType extends AbstractType
                 'label' => $this->__('URL of the Jenkins server'),
                 'help' => $this->__('Make sure to include "http://". Do not include "www". Example: "http://ci.zikula.org"')
             ])
-            ->add('jenkins_token', PasswordType::class, [
-                'always_empty' => false,
+            ->add('jenkins_token', TextType::class, [
                 'label' => $this->__('Jenkins Security token'),
                 'help' => $this->__f('A security token to verify requests from Jenkins. Please setup Jenkins to make a POST request to the following url everytime a build has finished: %s. You can use the "Post Completed Build Result Plugin" to do the job: https://wiki.jenkins-ci.org/display/JENKINS/Post+Completed+Build+Result+Plugin.', ['%s' => '<a href="' . $route . '">' . $route . '</a>'])
             ])
@@ -78,8 +74,7 @@ class ConfigType extends AbstractType
                 'label' => $this->__('Jenkins Username'),
                 'help' => $this->__('Must be set to the User ID found under the API Token section at your user account\'s settings.')
             ])
-            ->add('jenkins_password', PasswordType::class, [
-                'always_empty' => false,
+            ->add('jenkins_password', TextType::class, [
                 'label' => $this->__('Jenkins Password'),
                 'help' => $this->__('Must be set to the API Token found under the API Token section at your user account\'s settings .')
             ])
