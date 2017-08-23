e22bd18d41a79024e3835dc0024eb2d8955294b1
Craig Heydenburg <craigh@mac.com>
2017-08-23 10:05:08 -0400

    add AnyRelease block

---
+++ Block/AnyReleaseBlock.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2014 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license MIT
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Module\CoreManagerModule\Block;
+
+use Zikula\BlocksModule\AbstractBlockHandler;
+use Zikula\Module\CoreManagerModule\Block\Form\Type\AnyReleaseBlockType;
+use Zikula\Module\CoreManagerModule\Entity\CoreReleaseEntity;
+
+class AnyReleaseBlock extends AbstractBlockHandler
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function display(array $properties)
+    {
+        if (!$this->hasPermission('ZikulaCoreManagerModule:jenkinsBuild:', "$properties[title]::", ACCESS_OVERVIEW)) {
+            return "";
+        }
+        /** @var \Symfony\Bridge\Doctrine\ManagerRegistry $em */
+        $em = $this->get('doctrine')->getManager();
+        $release = $em->getRepository('ZikulaCoreManagerModule:CoreReleaseEntity')->find($properties['release']);
+
+        return $this->renderView('@ZikulaCoreManagerModule/Blocks/anyrelease.html.twig', [
+            'btnBlock' => $properties['btnBlock'],
+            'release' => $release,
+            'id' => uniqid()
+        ]);
+    }
+
+    public function getFormClassName()
+    {
+        return AnyReleaseBlockType::class;
+    }
+
+    public function getFormOptions()
+    {
+        $releaseManager = $this->get('zikula_core_manager_module.releasemanager');
+        /** @var CoreReleaseEntity[] $releases */
+        $releases = $releaseManager->getSignificantReleases(false);
+        $choices = [];
+        foreach ($releases as $release) {
+            if ($release->getState() === CoreReleaseEntity::STATE_SUPPORTED) {
+                $choices[$release->getName() . ' ' . $release->getSemver()] = $release->getId();
+            }
+        }
+
+        return ['choices' => $choices];
+    }
+}
---
+++ Block/Form/Type/AnyReleaseBlockType.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\Module\CoreManagerModule\Block\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnyReleaseBlockType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('release', ChoiceType::class, [
+                'choices' => $options['choices']
+            ])
+            ->add('btnBlock', CheckboxType::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'zikulacoremanagermodule_anyreleaseblock';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => [],
+        ]);
+    }
+}
