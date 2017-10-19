21c145768dcfe0edf40f3e61b8e2a07e30c6901f
Guite <info@guite.de>
2017-10-19 07:18:16 +0200

    markup improvements

--- Helper/CoreReleaseEntityHelper.php
+++ Helper/CoreReleaseEntityHelper.php
@@ -40,19 +40,19 @@ class CoreReleaseEntityHelper
     public function stateToText($state, $singularPlural = 'singular')
     {
         if ($singularPlural == 'singular') {
-            $translation = array (
+            $translation = [
                 CoreReleaseEntity::STATE_OUTDATED => $this->__('Outdated version'),
                 CoreReleaseEntity::STATE_DEVELOPMENT => $this->__('Development version'),
                 CoreReleaseEntity::STATE_PRERELEASE => $this->__('Prerelease'),
                 CoreReleaseEntity::STATE_SUPPORTED => $this->__('Supported version')
-            );
+            ];
         } else {
-            $translation = array (
+            $translation = [
                 CoreReleaseEntity::STATE_OUTDATED => $this->__('Outdated versions'),
                 CoreReleaseEntity::STATE_DEVELOPMENT => $this->__('Development versions'),
                 CoreReleaseEntity::STATE_PRERELEASE => $this->__('Prereleases'),
                 CoreReleaseEntity::STATE_SUPPORTED => $this->__('Supported versions')
-            );
+            ];
         }
 
         return $translation[$state];
@@ -66,15 +66,15 @@ class CoreReleaseEntityHelper
      */
     public function getNewsText(CoreReleaseEntity $coreReleaseEntity)
     {
-        $downloadLinks = "";
+        $downloadLinks = '';
         if (count($coreReleaseEntity->getAssets()) > 0) {
             $downloadLinkTpl = '<a href="%link%" class="btn btn-success btn-sm">%text%</a>';
             foreach ($coreReleaseEntity->getAssets() as $asset) {
                 $downloadLinks .= str_replace('%link%', $asset['download_url'], str_replace('%text%', $asset['name'], $downloadLinkTpl));
             }
         } else {
-            $downloadLinks .= "<div class=\"alert alert-warning\">" .
-                $this->__('Direct download links not yet available!') . "</div>";
+            $downloadLinks .= '<p class="alert alert-warning">' .
+                $this->__('Direct download links not yet available!') . '</p>';
         }
 
         return CoreReleaseEntity::NEWS_DESCRIPTION_START .
