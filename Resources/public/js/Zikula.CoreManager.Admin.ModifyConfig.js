b552b69a275afd9707a7b6ad622a701cd873a386
Axel Guckelsberger <info@guite.de>
2021-12-20 15:42:37 +0100

    minor JS update

--- Resources/public/js/Zikula.CoreManager.Admin.ModifyConfig.js
+++ Resources/public/js/Zikula.CoreManager.Admin.ModifyConfig.js
@@ -3,7 +3,7 @@
         $('#el-modify-config-form input[type="password"]').each(function () {
             $(this).wrap('<div class="input-group"></div>').after('<span class="input-group-addon show-password" style="cursor: pointer"><i class="fa fa-eye"></i></span>');
         });
-        $('#el-modify-config-form .show-password').live('click', function () {
+        $(document.body).on('click', '#el-modify-config-form .show-password', function () {
             $(this).prev().attr('type', 'text');
         });
     });
